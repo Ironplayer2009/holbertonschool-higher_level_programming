e47789ce27e9850b3df6ed55003b4500d3bd3ab5
Mister Aiden <aydinsulxayev@gmail.com>
2025-11-30 12:58:18 +0400

    Add tebahpla and remove_char_at scripts

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+index = 0
+for letter in range(122, 96, -1):
+    if index % 2 == 0:
+        print("{}".format(chr(letter)), end="")
+    else:
+        print("{}".format(chr(letter - 32)), end="")
+    index +=1
